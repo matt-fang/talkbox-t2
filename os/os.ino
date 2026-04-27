283f9d2887e00c6ae17453b08ec909c8cc372615
Matthew Fang <matthewfang.calif@gmail.com>
2026-04-26 21:26:07 -0700

    rename server as remote for clearer semantics + other fixes

--- os/os.ino
+++ os/os.ino
@@ -8,25 +8,35 @@
 
 const char* SSID   = "bingowireless2g_EXT";
 const char* PASS   = "draco10935";
-// const char* SERVER_IP = "10.0.0.47"; // mac mini (bingowireless2g) -3/22/26
-const char* SERVER_IP = "10.0.0.135"; // tiny talkbox (bingowireless2g_EXT) -4/20/26 | (bingowireless2g) -3/28/26
-const int   SERVER_AUDIO_PORT   = 6000;
-const int   SERVER_POT_PORT   = 5001;
+// const char* remote_IP = "10.0.0.47"; // mac mini (bingowireless2g) -3/22/26
+const char* REMOTE_IP = "10.0.0.135"; // tiny talkbox (bingowireless2g_EXT) -4/20/26 | (bingowireless2g) -3/28/26
+// const char* REMOTE_IP = "10.0.0.154"; // big talkbox (bingowireless2g_EXT) -4/21/26
+const int   REMOTE_AUDIO_PORT   = 6000;
+const int   REMOTE_POT_PORT   = 5001;
 
-String SERVER_AUDIO_IP_PORT_STRING = String(SERVER_IP) + ":" + String(SERVER_AUDIO_PORT);
-String SERVER_POT_IP_PORT_STRING = String(SERVER_IP) + ":" + String(SERVER_POT_PORT);
+const int   AUDIO_PORT = 6000;
+const int   POT_PORT = 5001;
+
+String REMOTE_AUDIO_IP_PORT_STRING = String(REMOTE_IP) + ":" + String(REMOTE_AUDIO_PORT);
+String REMOTE_POT_IP_PORT_STRING = String(REMOTE_IP) + ":" + String(REMOTE_POT_PORT);
 
 const int POT_PIN          = 34;
 const int POT_PERIOD_MS = 500;
 
-volatile unsigned long last_server_connect_time;
-bool is_connected_to_server = false;
+volatile unsigned long last_remote_connect_time;
+bool is_connected_to_remote_server = false;
+bool is_connected_to_remote_client = false;
 
 WiFiClient audio_client;
 WiFiClient pot_client;
 I2SStream in;
 
-StreamCopy copier_in(audio_client, in, 512);
+WiFiClient remote_audio_client;
+WiFiServer audio_server(AUDIO_PORT);
+I2SStream out;
+
+StreamCopy copier_in(audio_client, in, 1024);
+StreamCopy copier_out(out, remote_audio_client, 1024);
 
 void connectToWiFi() {
     WiFi.mode(WIFI_STA);
@@ -42,24 +52,37 @@ void connectToWiFi() {
     Serial.println(WiFi.localIP());
 }
 
-void connectToServer(String port = "both") {
+void connectToRemote(String port = "both") {
     if (port == "audio" || port == "both") {
-        while (!audio_client.connect(SERVER_IP, SERVER_AUDIO_PORT)) {
-            Serial.println("Connecting to server audio port... " + SERVER_AUDIO_IP_PORT_STRING);
+        while (!audio_client.connect(REMOTE_IP, REMOTE_AUDIO_PORT)) {
+            Serial.println("Connecting to remote audio port... " + REMOTE_AUDIO_IP_PORT_STRING);
             delay(1000);
         }
-        Serial.println("Connected to server audio");
-        last_server_connect_time = millis();
-        is_connected_to_server = true;
+        Serial.println("Connected to remote audio");
+        last_remote_connect_time = millis();
+        is_connected_to_remote_server = true;
     }
 
     if (port == "pot" || port == "both") {
-        while (!pot_client.connect(SERVER_IP, SERVER_POT_PORT)) {
-            Serial.println("Connecting to server pot port... " + SERVER_POT_IP_PORT_STRING);
+        while (!pot_client.connect(REMOTE_IP, REMOTE_POT_PORT)) {
+            Serial.println("Connecting to remote pot port... " + REMOTE_POT_IP_PORT_STRING);
             delay(1000);
         }
-        Serial.println("Connected to server pot");
+        Serial.println("Connected to remote pot");
     }
+}
+
+void setupServer(String type = "both") {
+    if (type == "audio" || type == "both") {
+        audio_server.begin();
+        Serial.println("Audio remote is live at port " + String(AUDIO_PORT));
+    }
+
+    // TODO
+    // if (type == "pot" || type == "both") {
+    //     pot_remote.begin();
+    //     Serial.println("Pot remote is live at port " + String(POT_PORT));
+    // }
 }
 
 void setupMic() {
@@ -77,12 +100,27 @@ void setupMic() {
     Serial.println("Mic is running");
 }
 
-// MARK: POT WORKS - JUST LAGGY ON SERVER -3/22/26
+void setupSpeaker() {
+    auto cfg_out = out.defaultConfig(TX_MODE);
+    cfg_out.sample_rate = 16000;
+    cfg_out.bits_per_sample = 32;
+    cfg_out.channels = 1;
+    cfg_out.pin_bck = 26;
+    cfg_out.pin_ws = 25; // lrck
+    cfg_out.pin_data = 22; // sd
+    cfg_out.port_no = 1;
+
+    out.begin(cfg_out);
+
+    Serial.println("Speaker is running");
+}
+
+// MARK: POT WORKS - JUST LAGGY ON remote -3/22/26
 void streamPot(void * pvParameters) {
     for (;;) {
-        if (!is_connected_to_server) {
+        if (!is_connected_to_remote_server) {
             Serial.println("Lost connection, reconnecting...");
-            connectToServer("pot");
+            connectToRemote("pot");
             vTaskDelay(pdMS_TO_TICKS(1000));
             continue;
         }
@@ -96,15 +134,15 @@ void streamPot(void * pvParameters) {
 
 void streamMic(void * pvParameters) {
     for (;;) {
-        if (!is_connected_to_server) {
+        if (!is_connected_to_remote_server) {
             Serial.println("Lost connection, reconnecting...");
-            connectToServer("audio");
+            connectToRemote("audio");
             vTaskDelay(pdMS_TO_TICKS(1000));
             continue;
         }
         
         copier_in.copy();
-        last_server_connect_time = millis();
+        last_remote_connect_time = millis();
 
         // Serial.println(bytesCopied);
 
@@ -115,18 +153,39 @@ void streamMic(void * pvParameters) {
     }
 }
 
+void readSpeaker(void * pvParameters) {
+    for (;;) {
+        if (!remote_audio_client || !remote_audio_client.connected()) {
+            remote_audio_client = audio_server.available();
+            Serial.println("Waiting for audio remote client...");
+
+            vTaskDelay(pdMS_TO_TICKS(1000));
+            continue;
+        }
+
+        if (!is_connected_to_remote_client) {
+            Serial.println("Found audio client");
+            is_connected_to_remote_client = true;
+        }
+        
+        copier_out.copy();
+    }
+}
+
 void manageWiFi(void * pvParameters) {
     for(;;) {
-        if (last_server_connect_time <= millis() - 5000 && is_connected_to_server) {
-            Serial.println("Frozen copier detected, closing client connection...");
-            audio_client.stop();
-            is_connected_to_server = false;
-        }
+        // if (last_remote_connect_time <= millis() - 5000 && is_connected_to_remote_server) {
+        //     Serial.println("Frozen copier detected, closing client connection...");
+        //     audio_client.stop();
+        //     is_connected_to_remote_server = false;
+        // }
 
         Serial.print("wifi strength: ");
         Serial.print(WiFi.RSSI());
-        Serial.print(", connected:");
-        Serial.println(audio_client.connected());
+        Serial.print(", connected to remote:");
+        Serial.print(audio_client.connected());
+        Serial.print(", remotes connected:");
+        Serial.println(remote_audio_client.connected());
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
@@ -136,12 +195,15 @@ void setup() {
     Serial.begin(115200);
 
     connectToWiFi();
-    connectToServer("audio");
+
     setupMic();
+    setupSpeaker();
+
+    setupServer("audio");
     
     xTaskCreatePinnedToCore(
         streamMic,
-        "Stream Mic to Server",
+        "Stream Mic to Remote",
         16000,
         NULL,
         1,
@@ -159,9 +221,19 @@ void setup() {
         1
     );
 
+    xTaskCreatePinnedToCore(
+        readSpeaker,
+        "Playback Client Mic in Speaker",
+        16000,
+        NULL,
+        1,
+        NULL,
+        0
+    );
+
     // xTaskCreatePinnedToCore(
     //     streamPot,
-    //     "Stream Pot to Server",
+    //     "Stream Pot to remote",
     //     4096,
     //     NULL,
     //     0,
